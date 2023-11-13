92ed006895b465d489df3e243fb2560c2cd53751
JuanmaVD <JuanmaVD21@gmail.com>
2023-11-13 20:21:42 -0300

    1

--- src/main/java/com/tup/buensabor/controllers/ClienteController.java
+++ src/main/java/com/tup/buensabor/controllers/ClienteController.java
@@ -7,13 +7,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "/clientes")
+@RequestMapping(path = "/api/clientes")
 public class ClienteController extends BaseControllerImpl<Cliente, ClienteServiceImpl> {
 
     @GetMapping("/searchRanking")
@@ -43,11 +44,41 @@ public class ClienteController extends BaseControllerImpl<Cliente, ClienteServic
         }
     }
     @GetMapping("/rankingPersonas")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<?> rankingClientes(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date desde, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)  Date hasta) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(servicio.rankingClientes(desde, hasta));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\": \"" + e.getMessage() + "\"}"));
         }
+    }
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<?> getAll() {
+        return super.getAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOne(Long id) {
+        return super.getOne(id);
+    }
+
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<?> save(Cliente entity) {
+        return super.save(entity);
+    }
+
+
+    @PutMapping("{id}")
+    public ResponseEntity<?> update(Long id, Cliente entity) {
+        return super.update(id, entity);
+    }
+
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> delete(Long id) {
+        return super.delete(id);
     }
 }
